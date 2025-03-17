5f077d4e57f14d6786259eb5a9e4546cc46d4856
Nathan Andrew <nathannxx@proton.me>
2025-03-18 02:41:32 +0700

    Update overlay.mk

--- overlay.mk
+++ overlay.mk
@@ -430,6 +430,8 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redminote12pro5g \
 	treble-overlay-xiaomi-redminote13pro4g \
 	treble-overlay-xiaomi-redminote13pro4g-systemui \
+        treble-overlay-xiaomi-redminote144g \
+        treble-overlay-xiaomi-redminote144g-systemui \
 	treble-overlay-xiaomi-redminote5 \
 	treble-overlay-xiaomi-redminote6pro \
 	treble-overlay-xiaomi-redminote6pro-systemui \
