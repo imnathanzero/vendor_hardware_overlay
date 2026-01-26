e61a076587af92f5e132249f4e058c889ea89c8d
Nathan <nathannxx@proton.me>
2026-01-26 22:55:19 +0700

    markifob

--- overlay.mk
+++ overlay.mk
@@ -1,3 +1,7 @@
 PRODUCT_PACKAGES += \
         treble-overlay-xiaomi-redminote144g-fwk \
-        treble-overlay-xiaomi-redminote144g-systemui
+        treble-overlay-xiaomi-redminote144g-spr \
+        treble-overlay-xiaomi-redminote144g-sr \
+        treble-overlay-xiaomi-redminote144g-sysui \
+        treble-overlay-xiaomi-redminote144g-tethering \
+        treble-overlay-xiaomi-redminote144g-wifi
